6d95da3fdd955a265c28e8721acd95d9ef277a38
Ronald <ronald.crump@captivea.com>
2021-04-20 22:04:37 -0700

    Code Update

--- cap_mrp_workcenters_stage/models/mrp_workcenter.py
+++ cap_mrp_workcenters_stage/models/mrp_workcenter.py
@@ -6,6 +6,13 @@ class MrpWorkcenter(models.Model):
     _inherit = ['mrp.workcenter', 'mail.activity.mixin']
 
     stage_id = fields.Many2one("mrp.workcenter.stage")
+    avg_target = fields.Float("Average Target", compute="_get_avg", store=True)
+
+    def _get_avg(self):
+        self = self.search([])
+        for rec in self:
+            recs = self.env['mrp.workcenter'].search([('stage_id', '=', rec.stage_id.id)])
+            rec.avg_target = rec.oee_target / len(recs) if len(recs) > 0 else 1
 
 
 class MrpWorkcenterStage(models.Model):
